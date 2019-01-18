b93c4e08b8df841a9ae911248aa161552813f542
bahrus <anderson.bruce.b@gmail.com>
2019-01-18 18:07:52 -0500

    ...

--- event-switch.ts
+++ event-switch.ts
@@ -21,7 +21,27 @@ export interface EventSwitchContext{
     eventSwitch: RuleMapping;
 }
 export function addEventListeners(target: EventTarget, ctx: EventSwitchContext){
-    for(var key in ctx){
-
+    for(const key in ctx.eventSwitch){
+        const rule = ctx.eventSwitch[key];
+        target.addEventListener(key, e =>{
+            processRule(rule, e, ctx);
+        })
+    }
+}
+function processRule(rule: Rule,  e: Event, ctx: EventSwitchContext){
+    if(rule.action !== undefined){
+        rule.action(e, ctx);
+    }
+    if(rule.route !== undefined){
+        for(const matchRuleKey in rule.route){
+            const matchRule = rule.route[matchRuleKey];
+            if(!matchRule.type) matchRule.type = 'targetMatch';
+            switch(matchRule.type){
+                case 'targetMatch':
+                    break;
+                case 'propMatch':
+                    break; 
+            }
+        }
     }
 }
