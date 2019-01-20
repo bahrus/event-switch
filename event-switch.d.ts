9edf63ad97c7369dd7e3bffc44bdd9901dc85235
bahrus <anderson.bruce.b@gmail.com>
2019-01-20 05:53:51 -0500

    ...

---
+++ event-switch.d.ts
@@ -0,0 +1,20 @@
+export type EventHandler = (e: Event) => undefined | EventSwitchContext;
+
+export type TestType = 'targetMatch' | 'propMatch';
+
+export interface Test{
+    type: TestType,
+    expression: string,
+}
+
+type RuleMapping = {[key: string] : Rule};
+
+export interface Rule extends Test{
+    action: (e: Event, ctx: EventSwitchContext) => undefined | Rule,
+    route: RuleMapping,
+}
+
+
+export interface EventSwitchContext{
+    eventSwitch: RuleMapping;
+}
--- event-switch.ts
+++ event-switch.ts
@@ -1,25 +1,5 @@
+import {Rule, EventSwitchContext} from './event-switch.d.js';
 
-export type EventHandler = (e: Event) => undefined | EventSwitchContext;
-
-export type TestType = 'targetMatch' | 'propMatch';
-
-export interface Test{
-    type: TestType,
-    expression: string,
-}
-
-type RuleMapping = {[key: string] : Rule};
-
-export interface Rule extends Test{
-    action: (e: Event, ctx: EventSwitchContext) => undefined | Rule,
-    route: RuleMapping,
-}
-
-//export type EventRouter = EventHandler | string;
-
-export interface EventSwitchContext{
-    eventSwitch: RuleMapping;
-}
 export function addEventListeners(target: EventTarget, ctx: EventSwitchContext){
     for(const key in ctx.eventSwitch){
         const rule = ctx.eventSwitch[key];
